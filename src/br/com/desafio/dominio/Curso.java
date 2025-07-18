ebe937c9f128daf3c99dc2eba3ea23204d9c1165
Kbatista7 <kbsilva7@outlook.pt>
2025-07-17 21:12:42 -0300

    Inclusão de arquivos

---
+++ src/br/com/desafio/dominio/Curso.java
@@ -0,0 +1,34 @@
+package br.com.desafio.dominio;
+
+public class Curso extends Conteudo {
+
+    private int    cargaHoraria;
+
+    @Override
+    public double calcularXp() {
+        return xp_padrao * cargaHoraria;
+    }
+
+    public Curso() {
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+
+
+}
